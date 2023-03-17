5feb79851dd092b2fdd11b6a9b1b16cb0052e85b
julisahun <juli.sahun@estudiantat.upc.edu>
2023-03-18 00:44:30 +0100

    pca analisis first step

---
+++ practica1/pca.r
@@ -0,0 +1,28 @@
+install.packages("corrr")
+install.packages("ggcorrplot")
+install.packages("FactoMineR")
+install.packages("factoextra")
+library(corrr)
+library(ggplot2)
+library(ggcorrplot)
+library(FactoMineR)
+library(factoextra)
+
+path <- "D:/uni/4r/MD-FIB/practica1/preprocessed_GPUs.csv"
+gpus <-
+numericBools <- gpus
+cols <- sapply(gpus, is.logical)
+numericBools[,cols] <- lapply(gpus[,cols], as.numeric)
+
+cols <- unlist(lapply(numericBools, is.numeric))
+numericGpu <- numericBools[,cols]
+
+normalizedGpus <- scale(numericGpu)
+
+corr_matrix <- cor(normalizedGpus)
+ggcorrplot(corr_matrix)
+
+data.pca <- princomp(corr_matrix)
+summary(data.pca)
+
+fviz_eig(data.pca, addlabels = TRUE)
